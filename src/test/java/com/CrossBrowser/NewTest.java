926ccd1068c9e283bb773d22c3a547b447ff01b0
kishoresasuke <kishoreragul123@gmail.com>
2024-05-12 10:47:01 +0530

    amazon second update

--- src/test/java/com/CrossBrowser/NewTest.java
+++ src/test/java/com/CrossBrowser/NewTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,12 +18,14 @@ public class NewTest {
 	public WebDriver driver;
 	@Parameters({"name"})
 	 @Test
-  public void CrossBrowser(String name) throws MalformedURLException {
+  public void CrossBrowser(String name) throws MalformedURLException, InterruptedException {
 	  
 	  if (name.equalsIgnoreCase("chrome")) {
 		  ChromeOptions op= new ChromeOptions();
 		  driver=new RemoteWebDriver(new URL("http://localhost:4444/"),op);
+		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		  driver.get("https://www.amazon.in/");
+		  Thread.sleep(10000);
 		  
 	  }
 	 
@@ -30,14 +33,17 @@ public class NewTest {
 		 EdgeOptions op= new EdgeOptions();
 		  driver=new RemoteWebDriver(new URL("http://localhost:4444/"),op);
 		  driver.get("https://www.amazon.in/");
+		  Thread.sleep(10000);
 	  }
 	else if (name.equalsIgnoreCase("firefox")) {
 		 FirefoxOptions op= new FirefoxOptions();
 		  driver=new RemoteWebDriver(new URL("http://localhost:4444/"),op);
 		  driver.get("https://www.amazon.in/");
-		
+		  Thread.sleep(10000);
 	}
+	  
 	  System.out.println(driver.getTitle());
+	  Thread.sleep(10000);
 	  
 	  
   }
